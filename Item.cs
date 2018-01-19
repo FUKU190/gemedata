fa9244f8675430b448341bb3228714bc8094b66a
Venn190 <fikuo0514@gmail.com>
2018-01-19 14:35:21 +0900

    ファーストコミット

---
+++ Item.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Item : MonoBehaviour {
+    public Text scoretext;
+   public int score = 0;
+    public AudioClip BallGet;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal =
+            Input.GetAxis("Horizontal");
+        float moveVertical =
+            Input.GetAxis("Vertical");
+
+        Vector3 movement = new
+            Vector3(moveHorizontal, 0.0f, moveVertical);
+	}
+     void OnTriggerEnter(Collider hit)
+    {
+        //ボールタグに触れるとスコア+100
+        if (hit.CompareTag("Ball"))
+        {
+            Destroy(hit.gameObject);
+            AudioSource.PlayClipAtPoint(BallGet, transform.position);
+            score += 100;
+            scoretext.text= "Score:"+score.ToString();
+            
+        }
+    }
+}
---
+++ Pause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public bool stopTime;
+    public GameObject ui;
+    private GameObject obj;
+    public CanvasGroup Pausecanvas;
+    public CanvasGroup Maincanvas;
+
+	// Use this for initialization
+	void Start () {
+        stopTime = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Gamestop()
+    {
+        //メニューボタンを押すとポーズ画面になり、時間が停止する。
+       
+        if (!stopTime)
+        {
+            obj = Instantiate(ui);
+            Time.timeScale = 0;
+            Pausecanvas.alpha = 1;
+            Maincanvas.alpha = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Pausecanvas.alpha = 0;
+            Maincanvas.alpha = 1;
+            Destroy(obj);
+           
+        }
+        stopTime = !stopTime;
+    }
+}

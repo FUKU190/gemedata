7afbfd8cdb12e7cf5c0afd11e87db345ac14c671
Venn190 <fikuo0514@gmail.com>
2018-01-19 14:34:31 +0900

    no message

---
+++ Active.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Active : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public Button button1;
+    public Text ResultScoreText;
+   
+    // Use this for initialization
+    void Start()
+    {
+        canvasGroup.alpha = 0;
+        //        button1.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider hit)
+    {
+        //ゴールタグに触れるとスコア表示
+        if (hit.CompareTag("Goal"))
+        {
+            
+          
+            canvasGroup.alpha = 1;
+            ResultScoreText.text = "Score:"+GameObject.Find("unitychan").GetComponent<Item>().score.ToString();
+
+           
+
+
+        }
+    }
+}

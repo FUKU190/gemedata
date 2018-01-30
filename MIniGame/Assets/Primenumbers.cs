902168ab3e884f051f8dc8a5368abad9e6945406
Venn190 <fikuo0514@gmail.com>
2018-01-30 13:49:04 +0900

    コミット

---
+++ MIniGame/Assets/Primenumbers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primenumbers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("デバッグログ");
+        Primenumbersout();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Primenumbersout()
+    {
+        for(int n = 2; n <= 101; n++){
+            if (n / 2 == 1)
+            {
+                Debug.Log(n);
+            }
+             if(n / 3 == 1)
+            {
+                Debug.Log(n);
+            }
+        }
+    }
+}

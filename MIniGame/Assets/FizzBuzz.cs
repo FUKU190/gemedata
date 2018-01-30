8cdd97733027e98dc8b87a5cf7bc3aa2dffb1fbd
Venn190 <fikuo0514@gmail.com>
2018-01-30 13:49:04 +0900

    コミット

---
+++ MIniGame/Assets/FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FizzBuzz : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("デバッグログ");
+        FizzBuzzoutput();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FizzBuzzoutput()
+    {
+        // public int n = 1
+        for (int n = 1; n <= 20; n++)
+        {
+            if(n % 3 == 0 && n % 5== 0)
+            {
+                Debug.Log("FizzBuzz");
+            }
+            else if(n % 3 == 0)
+            {
+                Debug.Log("Fizz");
+            }
+            else if(n % 5 == 0)
+            {
+                Debug.Log("Buzz");
+            }
+            else
+            {
+                Debug.Log(n);
+            }
+        }
+
+    }
+}
---
+++ MIniGame/Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    int num;
+    public GameObject cube;
+    public GameObject cube1;
+    public GameObject cube2;
+    public Transform SpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(cube, SpawnPoint);
+        }
+		
+	}
+}
